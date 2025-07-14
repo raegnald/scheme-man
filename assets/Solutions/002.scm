360f3effb0be343020d67bbac4dbfe8255b17e74
Pau Sánchez <pausl19@MacBook-Air-de-Pau.local>
2025-07-14 18:20:36 +0200

    Cambios Pau

---
+++ assets/Solutions/002.scm
@@ -0,0 +1,17 @@
+;; Posicionarme
+(walk)
+(turn 'right)
+(walk)
+
+(status "Me lo paso a la primera")
+
+;; Completar nivel
+(repeat 3
+        (walk 2)
+        (repeat 2
+                (turn 'left)
+                (walk 2))
+        (turn 'right)
+        (walk 2)
+        (turn 'right))
+(walk 2)
